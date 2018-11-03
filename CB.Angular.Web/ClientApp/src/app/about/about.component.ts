5d63afaa9358b94d7e02d17e13013cfc5f93756d
Adrian Brink <adrian.brink@outlook.com>
2018-11-03 19:00:14 +0200

    updated github alerts

--- CB.Angular.Web/ClientApp/src/app/about/about.component.ts
+++ CB.Angular.Web/ClientApp/src/app/about/about.component.ts
@@ -1,5 +1,4 @@
-import { Component, Inject } from '@angular/core';
-import { Http } from '@angular/http';
+import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 
 import { Profile } from '../../models/profile';
